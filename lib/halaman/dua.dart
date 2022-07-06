dbd70919703f25326e0d0cac489c704436c85198
penaa9 <trivena.natalia2015@gmail.com>
2022-07-06 17:33:58 +0700

    edit halaman satu dan dua isi yang sama

---
+++ lib/halaman/dua.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DuaHalaman extends StatefulWidget {
+  const DuaHalaman({Key? key}) : super(key: key);
+
+  @override
+  State<DuaHalaman> createState() => _DuaHalamanState();
+}
+
+class _DuaHalamanState extends State<DuaHalaman> {
+  @override
+  Widget build(BuildContext context) {
+    
+  }
+}
---
+++ lib/halaman/satu.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SatuHalaman extends StatefulWidget {
+  const SatuHalaman({Key? key}) : super(key: key);
+
+  @override
+  State<SatuHalaman> createState() => _SatuHalamanState();
+}
+
+class _SatuHalamanState extends State<SatuHalaman> {
+  @override
+  Widget build(BuildContext context) {
+    
+  }
+}
